68a78580fbb715238a567c42a13b9925d3657939
Luis A. Padre Garcia <pagl@praxisit.local>
2020-01-09 18:15:07 -0600

    Reportes

--- src/Pages/Auth/Constructoras/Reportes/Filtros.jsx
+++ src/Pages/Auth/Constructoras/Reportes/Filtros.jsx
@@ -1,7 +1,12 @@
 import React, { useState } from 'react';
 import { Collapse, CardBody, Card } from 'reactstrap';
-import { Button } from 'antd';
-import { Column, Tag } from 'rbx'
+import { Column, Tile, Title, Content, Notification, Container } from 'rbx'
+import { Badge } from 'reactstrap';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { AutoComplete, DatePicker, Button } from 'antd';
+
+
 
 export default function Filtros({ children }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -9,27 +14,156 @@ export default function Filtros({ children }) {
   const toggle = () => setIsOpen(!isOpen);
 
   return (
-    <div >
-      <Column.Group multiline centered breakpoint="mobile">
-        <Column>
-          <Button type="primary" size={'large'} onClick={toggle} style={{ marginBottom: '1rem' }}>
-            Graficar por Filtros
-      </Button></Column>
-        <Column style={{background:'#fff'}}>
-          Compras TOTALES
-      </Column>
-      </Column.Group>
+    <div style={{ background: '#fff' }} >
+      <TITLESs toggle={toggle} />
       <Collapse isOpen={isOpen}>
         <Card>
           <CardBody>
-            Anim pariatur cliche reprehenderit,
-             enim eiusmod high life accusamus terry richardson ad squid. Nihil
-             anim keffiyeh helvetica, craft beer labore wes anderson cred
-             nesciunt sapiente ea proident.
+
+          <Notification  textAlign="centered">
+
+            <Tile kind="ancestor">
+              <Tile kind="parent">
+                <Tile as={Box} kind="child">
+                  <Calendar label="Filtar Por Mes:" placeholder="Selecciona un Mes" />
+                </Tile>
+              </Tile>
+              <Tile kind="parent">
+                <Tile as={Box} kind="child">
+
+                  <AutoCompletar dataSource={Pais} placeholder="Escribe un Pais" label="Pais" />
+                </Tile>
+              </Tile>
+              <Tile kind="parent">
+                <Tile as={Box} kind="child">
+                  <AutoCompletar dataSource={Estado} placeholder="Escribe un Estado" label="Estado" />
+                </Tile>
+              </Tile>
+            </Tile>
+</Notification>
+
+            <Column.Group>
+              <Column>
+                <Notification  textAlign="centered">
+                  <AutoCompletar dataSource={Proveedor} placeholder="Escribe un Proveedor" label="Proveedor" />
+                </Notification>
+              </Column>
+              <Column>
+                <Notification  textAlign="centered">
+                  <AutoCompletar dataSource={Material} placeholder="Escribe un Material" label="Materiales" />
+                </Notification>
+              </Column>
+
+            </Column.Group>
+
           </CardBody>
         </Card>
       </Collapse>
       {children}
     </div>
   );
+}
+
+const TITLESs = ({ toggle }) => {
+  return (<Container>
+    <Tile kind="ancestor">
+      <Tile kind="parent">
+        <Tile as={Box} kind="child">
+          <Box fontSize="h6.fontSize">Compras Totales al Mes: <Badge color="secondary">8</Badge></Box>
+        </Tile>
+      </Tile>
+      <Tile kind="parent">
+        <Tile as={Box} kind="child">
+          <Box fontSize="h6.fontSize">Compras Totales al Año: <Badge color="secondary">12</Badge></Box>
+        </Tile>
+      </Tile>
+      <Tile kind="parent">
+        <Tile as={Box} kind="child">
+          <Box fontSize="h6.fontSize">Compras Totales a la Semana: <Badge color="secondary">6</Badge></Box>
+        </Tile>
+      </Tile>
+    </Tile>
+    <Tile kind="ancestor">
+      <Tile vertical size={9}>
+        <Tile>
+          <Tile kind="parent">
+            <Tile as={Box} kind="child">
+              <Button type="primary" size={'large'} onClick={toggle} style={{ marginBottom: '1rem' }}>
+                Graficar por Filtros
+            </Button>
+            </Tile>
+          </Tile>
+        </Tile>
+      </Tile>
+    </Tile>
+  </Container>)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const { MonthPicker } = DatePicker;
+
+function Calendar({ label, placeholder }) {
+
+  function onChange(date, dateString) {
+    console.log(date, dateString);
+  }
+  return (
+    <Box fontSize="h6.fontSize">{label} <MonthPicker onChange={onChange} placeholder={placeholder} /></Box>
+  )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const Pais = ['Mexico', 'Colombia', 'Peru', 'Argentina']
+const Estado = ['Hidalgo', 'Oaxaca', 'Estado de Mexico', 'Queretaro', 'Monterrey']
+const Proveedor = ['Jose', 'Misael', 'Antonio', 'Fernando']
+const Material = ['Cemento', 'Carretilla']
+
+
+
+
+
+function AutoCompletar({ dataSource, placeholder, label }) {
+  return (
+    <Box fontSize="h6.fontSize">{label}: <AutoComplete
+      dataSource={dataSource}
+      style={{ width: 200 }}
+      placeholder={placeholder}
+      filterOption={(inputValue, option) =>
+        option.props.children.toUpperCase().indexOf(inputValue.toUpperCase()) !== -1
+      }
+    /></Box>
+  )
 }
--- src/Pages/Auth/Constructoras/Reportes/index.jsx
+++ src/Pages/Auth/Constructoras/Reportes/index.jsx
@@ -7,18 +7,21 @@ import { Line } from 'react-chartjs-2';
 
 const Semana = ['Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado', 'Domingo']
 const DataSemana = [12, 13, 342, 234, 564, 756, 234]
-const Mes = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+const DataSemana2 = [2, 3, 4, 2, 6, 5, 4]
+const Mes = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
 const DataMes = [12, 12, 12, 3432, 1345, 123, 2353, 23, 123, 123, 123, 123]
+const DataMes2 = [1, 2, 13, 32, 13, 23, 53, 23, 12, 13, 23, 12]
 const Año = ['2018', '2019', '2020']
 const DataAño = [1212, 1211, 1219]
+const DataAño2 = [112, 121, 100]
 
-const OtherData = (label, data) => {
+const OtherData = (label, data, color) => {
 
     return {
         fill: false,
         lineTension: 0.1,
         backgroundColor: 'rgba(75,192,192,0.4)',
-        borderColor: 'rgba(75,192,192,1)',
+        borderColor: color,
         borderCapStyle: 'butt',
         borderDash: [],
         borderDashOffset: 0.0,
@@ -37,12 +40,16 @@ const OtherData = (label, data) => {
     }
 }
 
-const CrearGrafica = ({ Tipo, Datos }) => {
-    var label = 'Compras Totales'
+const CrearGrafica = ({ Tipo, Datos, Datos2 }) => {
+    var label = 'Compras'
+    var label2 = 'Cotizaciones'
+    var colo1 = '#273c75'
+    var colo2 = '#e84118'
     const lineData = {
         labels: Tipo,
         datasets: [
-            OtherData(label, Datos),
+            OtherData(label, Datos, colo1),
+            OtherData(label2, Datos2, colo2),
         ]
     }
     return (
@@ -53,25 +60,25 @@ export default function () {
     return (<>
         <Filtros>
             <Hero color="white">
-                <Hero.Body style={{ padding: 'initial "!important' }}>
+                <Hero.Body >
                     <Container>
                         <Column.Group multiline centered breakpoint="mobile">
                             <Column>
                                 <CardReports >
                                     <Title>Semana</Title>
-                                    <CrearGrafica Tipo={Semana} Datos={DataSemana} />
+                                    <CrearGrafica Tipo={Semana} Datos={DataSemana} Datos2={DataSemana2} />
                                 </CardReports>
                             </Column>
                             <Column>
                                 <CardReports>
                                     <Title>Mes</Title>
-                                    <CrearGrafica Tipo={Mes} Datos={DataMes} />
+                                    <CrearGrafica Tipo={Mes} Datos={DataMes} Datos2={DataMes2} />
                                 </CardReports>
                             </Column>
                             <Column size={12}>
                                 <CardReports>
                                     <Title>Año</Title>
-                                    <CrearGrafica Tipo={Año} Datos={DataAño} />
+                                    <CrearGrafica Tipo={Año} Datos={DataAño} Datos2={DataAño2} />
                                 </CardReports>
                             </Column>
                         </Column.Group>
